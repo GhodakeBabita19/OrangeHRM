6017dad482d71dd1d171eedf7c92bc501cbc7eab
Vinod <ghodakebabita09@gmail.com>
2024-06-13 16:43:55 +0530

    git commit -m "Added forgot functionality"

--- src/main/java/com/utility/Utility.java
+++ src/main/java/com/utility/Utility.java
@@ -5,20 +5,19 @@ import org.openqa.selenium.WebElement;
 import com.baseclass.BaseClass;
 
 public class Utility extends BaseClass {
-	
+
 	public static String getWebElementText(WebElement ele) {
-		
+
 		return ele.getText();
 	}
 
-	public static void setText(WebElement ele,String text) {
+	public static void setText(WebElement ele, String text) {
 		ele.sendKeys(text);
-		
+
 	}
-	
-	public static void  webelementClick(WebElement ele) {
+
+	public static void webelementClick(WebElement ele) {
 		ele.click();
 	}
-	 
-	
+
 }
